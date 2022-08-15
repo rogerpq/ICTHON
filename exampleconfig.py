d1bdcc4121e4a312ea8e1115a300c08ce8f92e43
Nerooobot <111285329+Nerooobot@users.noreply.github.com>
2022-08-15 06:51:36 -0700

    Add files via upload

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,16 +2,23 @@ from sample_config import Config
 
 
 class Development(Config):
-    #هنا ضع الكودات الخاصه بك امسح الكلام العربي وضعهن
+    # get this values from the my.telegram.org
     APP_ID = 6
-    API_HASH = "هنا الايبي هاش"
-    ALIVE_NAME = "اسم حسابك"
-    DB_URI = "رابط تخزينك"
-    STRING_SESSION = "كود تيرمكس"
-    TG_BOT_TOKEN = "توكن بوتك"
-    TG_BOT_USERNAME = "معرف بوتك بدون @"
+    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    # the name to display in your alive message
+    ALIVE_NAME = "Your value"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "Your value"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    STRING_SESSION = "Your value"
+    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    TG_BOT_TOKEN = "Your value"
+    TG_BOT_USERNAME = "Your value"
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
+    # command handler
     COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
     SUDO_USERS = []
-    SUDO_COMMAND_HAND_LER = "،"
-    TZ = "Asia/Baghdad"
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
