c4e1b009d71471c32833f7e1af5756a90075fe6f
Nerooobot <111285329+Nerooobot@users.noreply.github.com>
2022-08-15 17:40:38 +0300

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -21,8 +21,8 @@ StartTime = time.time()
 JMVERSION = "5.2.0"
 
 
-if Config.UPSTREAM_REPO == "jepthoniq":
-    UPSTREAM_REPO_URL = "https://github.com/jepthoniq/jepthon"
+if Config.UPSTREAM_REPO == "icthonar":
+    UPSTREAM_REPO_URL = "https://github.com/Nerooobot/ICTHON"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
