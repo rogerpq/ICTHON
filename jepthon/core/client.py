09e2b5f7db2a79d3ae6de77471c8fba669443c4a
Nerooobot <111285329+Nerooobot@users.noreply.github.com>
2022-08-18 09:47:52 +0300

    Update client.py

--- jepthon/core/client.py
+++ jepthon/core/client.py
@@ -141,7 +141,7 @@ class CatUserBotClient(TelegramClient):
                             ftext, pastetype="s", markdown=False
                         )
                         text = "**تقرير خطا ايكثون**\n\n"
-                        link = "[هنا](https://t.me/ICTHON)"
+                        link = "[هنا](https://t.me/ICTHONN)"
                         text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
                         text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += "لا يتم تسجيل اي خطا فقط التاريخ والوقت\n\n"
@@ -256,7 +256,7 @@ class CatUserBotClient(TelegramClient):
                             ftext, pastetype="s", markdown=False
                         )
                         text = "**تقرير خطا ايكثون**\n\n"
-                        link = "[هنا](https://t.me/ICTHON)"
+                        link = "[هنا](https://t.me/ICTHONN)"
                         text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
                         text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += "لا يتم تسجيل اي خطا فقط التاريخ والوقت\n\n"
